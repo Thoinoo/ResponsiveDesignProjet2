eeb0ee536574d2d7b48316c40786a7c6d9428e42
Thoinoo <thoino@outlook.fr>
2022-09-17 17:44:47 +0200

    ajout des fonctions hover et icones social network

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
 function openNav() {
-  if (document.getElementById("mySidenav").style.width == "85vw" )
+  if (document.getElementById("mySidenav").style.width == "75vw" )
   {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
   }
   else
   {
-    document.getElementById("mySidenav").style.width = "85vw";
-    document.getElementById("main").style.marginLeft = "85vw";
+    document.getElementById("mySidenav").style.width = "75vw";
+    document.getElementById("main").style.marginLeft = "75vw";
   }
   
 }
