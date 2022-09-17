370cf77963c24488488d79e60b4705b89b3a9dad
Thoinoo <thoino@outlook.fr>
2022-09-17 10:15:49 +0200

    finition boutton navbar7

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
 function openNav() {
-  document.getElementById("mySidenav").style.width = "85vw";
-  document.getElementById("main").style.marginLeft = "85vw";
+  if (document.getElementById("mySidenav").style.width == "85vw" )
+  {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+  }
+  else
+  {
+    document.getElementById("mySidenav").style.width = "85vw";
+    document.getElementById("main").style.marginLeft = "85vw";
+  }
+  
 }
 
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
