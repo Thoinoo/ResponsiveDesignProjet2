c5ac72a54891eb3b5e6f22751a7f920913ac3e13
Thoinoo <thoino@outlook.fr>
2022-09-21 23:46:14 +0200

    mise en page SASS

--- app.js
+++ app.js
@@ -1,20 +1,43 @@
+let closed = true;
+
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
-function openNav() {
-  if (document.getElementById("mySidenav").style.width == "75vw" )
-  {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
+function openNav(e) {
+  if(closed) {
+    /*
+    if (document.getElementById("mySidenav").style.width == "75vw" )
+    {
+      document.getElementById("mySidenav").style.width = "0";
+      document.getElementById("main").style.marginLeft = "0";
+    }
+    else
+    {
+      document.getElementById("mySidenav").style.width = "75vw";
+      document.getElementById("main").style.marginLeft = "75vw";
+    }*/
+
+    if (document.getElementById("mySidenav").style.left == "0")
+    {
+      document.getElementById("mySidenav").style.left = "-90VW";
+      document.getElementById("main").style.left = "0";
+    }
+    else
+    {
+      document.getElementById("mySidenav").style.left = "0";
+      document.getElementById("main").style.left = "90VW";
+    }
+    setTimeout(() => {closed = false; /*console.log('opened')*/}, 300)
   }
-  else
-  {
-    document.getElementById("mySidenav").style.width = "75vw";
-    document.getElementById("main").style.marginLeft = "75vw";
-  }
-  
 }
 
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
 function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
-  document.getElementById("main").style.marginLeft = "0";
+  if(!closed) {
+    document.getElementById("mySidenav").style.left = "-90vw";
+    document.getElementById("main").style.left = "0";
+    setTimeout(() => {closed = true; console.log('closed')}, 300)
+  }
 } 
+
+
+document.getElementById('main').addEventListener("click", closeNav);
+document.getElementById('menuButton').addEventListener("click", openNav);
